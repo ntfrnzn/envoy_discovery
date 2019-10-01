230ae49c52f33748472e15b67703a06f12131b6a
Nate Franzen <nfranzen+github@gmail.com>
2019-09-30 18:16:18 -0700

    add tcp listener

--- discover.go
+++ discover.go
@@ -4,30 +4,48 @@ import (
 	"time"
 
 	"github.com/envoyproxy/go-control-plane/pkg/cache"
+	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
 	"github.com/golang/protobuf/ptypes"
 
 	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	endpoint "github.com/envoyproxy/go-control-plane/envoy/api/v2/endpoint"
+	listener "github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
+	tcp "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/tcp_proxy/v2"
+	"github.com/golang/protobuf/ptypes/any"
 )
 
 const (
-	EdsCluster = "eds_cluster"
+	XdsCluster = "xds_cluster"
 )
 
 func Discover() cache.Snapshot {
 	// return cache.Snapshot{}
 
-	clusterName := "k8s_api"
-	host := "10.0.0.200"
-	var port uint32 = 57116
+	clusterName := "vYeiGyJc9Z88"
+	sniHost := "k8s.vYeiGyJc9Z88.ntfrnzn.dev"
 
-	// version := time.Now().Format(time.RFC3339)
-	version := "v1"
-	endpoints := []cache.Resource{MakeEndpoint(clusterName, host, port)}
+	endpointHHost := "10.0.0.200"
+	var endpointPort uint32 = 62953
+
+	listenerHost := "0.0.0.0"
+	listenerName := "k8s-proxy"
+	var listenerPort uint32 = 6443
+
+	version := time.Now().Format(time.RFC3339)
+	//version := "0"
+
+	endpoints := []cache.Resource{MakeEndpoint(clusterName, endpointHHost, endpointPort)}
 	clusters := []cache.Resource{MakeCluster(clusterName)}
 	routes := []cache.Resource{}
-	listeners := []cache.Resource{}
+
+	tcpListener := MakeTCPListener(listenerName, listenerHost, listenerPort)
+	err := AddFilterChain(clusterName, sniHost, tcpListener)
+	if err != nil {
+		panic(err)
+	}
+	listeners := []cache.Resource{tcpListener}
+
 	return cache.NewSnapshot(
 		version,
 		endpoints,
@@ -73,7 +91,7 @@ func MakeCluster(clusterName string) *v2.Cluster {
 				SetNodeOnFirstMessageOnly: true,
 				GrpcServices: []*core.GrpcService{{
 					TargetSpecifier: &core.GrpcService_EnvoyGrpc_{
-						EnvoyGrpc: &core.GrpcService_EnvoyGrpc{ClusterName: EdsCluster},
+						EnvoyGrpc: &core.GrpcService_EnvoyGrpc{ClusterName: XdsCluster},
 					},
 				}},
 			},
@@ -88,5 +106,64 @@ func MakeCluster(clusterName string) *v2.Cluster {
 		EdsClusterConfig: &v2.Cluster_EdsClusterConfig{
 			EdsConfig: edsSource,
 		},
+	}
+}
+
+// AddFilterChain appends a cluser and SNI hostname to the TcpListener
+func AddFilterChain(clusterName, sniHost string, tcpListener *v2.Listener) error {
+
+	// TCP filter configuration
+	config := &tcp.TcpProxy{
+		StatPrefix: "tcp",
+		ClusterSpecifier: &tcp.TcpProxy_Cluster{
+			Cluster: clusterName,
+		},
+	}
+	pbst, err := ptypes.MarshalAny(config)
+	if err != nil {
+		return err
+	}
+	tcpListener.FilterChains = append(
+		tcpListener.FilterChains,
+		&listener.FilterChain{
+			FilterChainMatch: &listener.FilterChainMatch{
+				ServerNames: []string{sniHost},
+			},
+			Filters: []*listener.Filter{{
+				Name: wellknown.TCPProxy,
+				ConfigType: &listener.Filter_TypedConfig{
+					TypedConfig: pbst,
+				},
+			}},
+		},
+	)
+	return nil
+}
+
+// MakeTCPListener creates a TCP listener for a cluster with SNI indication
+func MakeTCPListener(listenerName string, host string, port uint32) *v2.Listener {
+
+	return &v2.Listener{
+		Name: listenerName,
+		Address: &core.Address{
+			Address: &core.Address_SocketAddress{
+				SocketAddress: &core.SocketAddress{
+					Protocol: core.SocketAddress_TCP,
+					Address:  host,
+					PortSpecifier: &core.SocketAddress_PortValue{
+						PortValue: port,
+					},
+				},
+			},
+		},
+		ListenerFilters: []*listener.ListenerFilter{
+			&listener.ListenerFilter{
+				Name: wellknown.TlsInspector,
+				ConfigType: &listener.ListenerFilter_TypedConfig{
+					TypedConfig: &any.Any{},
+				},
+			},
+		},
+		FilterChains: []*listener.FilterChain{},
 	}
 }
